e2fc503f10fb75deb03a35567e484c2091676ac2
sadiqeen <finixlllsarawut@gmail.com>
2021-03-07 21:46:26 +0700

    add pertner section

---
+++ front/components/partner.js
@@ -0,0 +1,20 @@
+import {
+    Row,
+    Col,
+    Image
+} from "react-bootstrap";
+import Fade from 'react-reveal/Fade';
+
+export default function () {
+    return (
+        <div className="mt-5">
+            <Fade bottom>
+                <h2 className="h1 text-center mt-5">Our Partner</h2>
+            </Fade>
+
+            <div className="text-center">
+                <Image src="/dark2_xvo1ep.webp" fluid className="mt-3" width="250"></Image>
+            </div>
+        </div>
+    )
+}
